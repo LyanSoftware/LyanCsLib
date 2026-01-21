a9c0d7341b51ab98c90f200b110f7da0b39c6407
K <970227268@qq.com>
2026-01-21 11:17:19 +0800

    添加单元测试

---
+++ WinFormsTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Lytec.WinForms;
+using Lytec.Image.GDIPlus;
+
+namespace WinFormsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
