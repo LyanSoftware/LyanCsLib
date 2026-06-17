d7ec474840d29b3c5543337111ea9f01b18364bc
K <970227268@qq.com>
2026-06-17 20:27:38 +0800

    [Common.Generators]移除会引发警告的未使用代码

--- Common.Generators/src/AutoSerializeGeneratorAnalyzer.cs
+++ Common.Generators/src/AutoSerializeGeneratorAnalyzer.cs
@@ -13,41 +13,41 @@ public class AutoSerializeGeneratorAnalyzer //  : DiagnosticAnalyzer
 {
     internal static class DiagnosticDescriptors
     {
-        public static readonly DiagnosticDescriptor AutoSerializeObjectMissingNoArgsConstructor = new(
-            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_001",
-            title: "用法错误",
-            messageFormat: "使用[AutoSerializeObject]的类型 {0} 必须具有无参数构造函数",
-            category: "Usage",
-            defaultSeverity: DiagnosticSeverity.Error,
-            isEnabledByDefault: true,
-            description: "使用[AutoSerializeObject]的类型必须具有无参数构造函数"
-            );
+//        public static readonly DiagnosticDescriptor AutoSerializeObjectMissingNoArgsConstructor = new(
+//            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_001",
+//            title: "用法错误",
+//            messageFormat: "使用[AutoSerializeObject]的类型 {0} 必须具有无参数构造函数",
+//            category: "Usage",
+//            defaultSeverity: DiagnosticSeverity.Error,
+//            isEnabledByDefault: true,
+//            description: "使用[AutoSerializeObject]的类型必须具有无参数构造函数"
+//            );
 
-        public static readonly DiagnosticDescriptor AutoSerializeObjectNotPartial = new(
-            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_002",
-            title: "语法错误",
-            messageFormat: "使用[AutoSerializeObject]的类型 {0} 必须是 partial 的",
-            category: "Syntax",
-            defaultSeverity: DiagnosticSeverity.Error,
-            isEnabledByDefault: true,
-            description: "使用[AutoSerializeObject]的类型必须是 partial 的"
-            );
+//        public static readonly DiagnosticDescriptor AutoSerializeObjectNotPartial = new(
+//            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_002",
+//            title: "语法错误",
+//            messageFormat: "使用[AutoSerializeObject]的类型 {0} 必须是 partial 的",
+//            category: "Syntax",
+//            defaultSeverity: DiagnosticSeverity.Error,
+//            isEnabledByDefault: true,
+//            description: "使用[AutoSerializeObject]的类型必须是 partial 的"
+//            );
         
-        public static readonly DiagnosticDescriptor NotSupportedMember = new(
-            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_003",
-            title: "用法错误",
-            messageFormat: @"成员 {0} 必须是满足以下条件之一，才能使用[AutoSerialize]：
-是基元类型/枚举/数组
-是[AutoSerializeObject]
-实现了IAutoSerializeObject<T>，才能自动使用[AutoSerialize]",
-            category: "Usage",
-            defaultSeverity: DiagnosticSeverity.Error,
-            isEnabledByDefault: true,
-            description: @"成员必须是满足以下条件之一，才能使用[AutoSerialize]：
-是基元类型/枚举/数组
-是[AutoSerializeObject]
-实现了IAutoSerializeObject<T>，才能自动使用[AutoSerialize]"
-            );
+//        public static readonly DiagnosticDescriptor NotSupportedMember = new(
+//            id: "LYTEC_COMMON_DATA_AUTO_SERIALIZE_003",
+//            title: "用法错误",
+//            messageFormat: @"成员 {0} 必须是满足以下条件之一，才能使用[AutoSerialize]：
+//是基元类型/枚举/数组
+//是[AutoSerializeObject]
+//实现了IAutoSerializeObject<T>，才能自动使用[AutoSerialize]",
+//            category: "Usage",
+//            defaultSeverity: DiagnosticSeverity.Error,
+//            isEnabledByDefault: true,
+//            description: @"成员必须是满足以下条件之一，才能使用[AutoSerialize]：
+//是基元类型/枚举/数组
+//是[AutoSerializeObject]
+//实现了IAutoSerializeObject<T>，才能自动使用[AutoSerialize]"
+//            );
         
     }
 
