cac4437c2e33e92f90238c48042fb2f589f5da2b
K <970227268@qq.com>
2026-06-25 18:47:07 +0800

    [Image]添加序列化/反序列化支持

--- Image/src/Color.cs
+++ Image/src/Color.cs
@@ -3,48 +3,47 @@ using System.Collections.Generic;
 using System.Text;
 using SkiaSharp;
 
-namespace Lytec.Image
+namespace Lytec.Image;
+
+public readonly partial struct Color : IEquatable<Color>
 {
-    public readonly partial struct Color : IEquatable<Color>
+    public byte R { get; }
+    public byte G { get; }
+    public byte B { get; }
+    public byte A { get; }
+
+    public SKColor SKColor => new SKColor(R, G, B, A);
+
+    public Color(byte r, byte g, byte b, byte a = 255) => (R, G, B, A) = (r, g, b, a);
+    public Color(SKColor color) : this(color.Red, color.Green, color.Blue, color.Alpha) { }
+    public Color(Color color) : this(color.R, color.G, color.B, color.A) { }
+    public void Deconstruct(out byte r, out byte g, out byte b, out byte a) => (r, g, b, a) = (R, G, B, A);
+
+    public static implicit operator SKColor(Color color) => color.SKColor;
+    public static implicit operator Color(SKColor color) => new(color);
+
+    public Color(string colorName)
     {
-        public byte R { get; }
-        public byte G { get; }
-        public byte B { get; }
-        public byte A { get; }
-
-        public SKColor SKColor => new SKColor(R, G, B, A);
-
-        public Color(byte r, byte g, byte b, byte a = 255) => (R, G, B, A) = (r, g, b, a);
-        public Color(SKColor color) : this(color.Red, color.Green, color.Blue, color.Alpha) { }
-        public Color(Color color) : this(color.R, color.G, color.B, color.A) { }
-        public void Deconstruct(out byte r, out byte g, out byte b, out byte a) => (r, g, b, a) = (R, G, B, A);
-
-        public static implicit operator SKColor(Color color) => color.SKColor;
-        public static implicit operator Color(SKColor color) => new(color);
-
-        public Color(string colorName)
-        {
-            if (!Colors.TryGetValue(colorName, out var c))
-                throw new ArgumentException("Unrecognized color name", nameof(colorName));
-            R = c.R;
-            G = c.G;
-            B = c.B;
-            A = c.A;
-        }
-        public static Color FromName(string colorName) => new(colorName);
-        public static bool TryFromName(string colorName, out Color color) => Colors.TryGetValue(colorName, out color);
-
-        public override bool Equals(object? obj) => obj is Color color && Equals(color);
-
-        public bool Equals(Color other)
-            => R == other.R &&
-               G == other.G &&
-               B == other.B &&
-               A == other.A;
-
-        public override int GetHashCode() => HashCode.Combine(R, G, B, A);
-
-        public static bool operator ==(Color left, Color right) => left.Equals(right);
-        public static bool operator !=(Color left, Color right) => !(left == right);
+        if (!Colors.TryGetValue(colorName, out var c))
+            throw new ArgumentException("Unrecognized color name", nameof(colorName));
+        R = c.R;
+        G = c.G;
+        B = c.B;
+        A = c.A;
     }
+    public static Color FromName(string colorName) => new(colorName);
+    public static bool TryFromName(string colorName, out Color color) => Colors.TryGetValue(colorName, out color);
+
+    public override bool Equals(object? obj) => obj is Color color && Equals(color);
+
+    public bool Equals(Color other)
+        => R == other.R &&
+           G == other.G &&
+           B == other.B &&
+           A == other.A;
+
+    public override int GetHashCode() => HashCode.Combine(R, G, B, A);
+
+    public static bool operator ==(Color left, Color right) => left.Equals(right);
+    public static bool operator !=(Color left, Color right) => !(left == right);
 }
--- Image/src/ImageData.cs
+++ Image/src/ImageData.cs
@@ -2,59 +2,97 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using Lytec.Common.Data;
 using SkiaSharp;
 
-namespace Lytec.Image
+namespace Lytec.Image;
+
+public class ImageData : IReadOnlyList<Color>
 {
-    public class ImageData : IReadOnlyList<Color>
+    public int Width { get; }
+    public int Height { get; }
+    public Color[] Pixels { get; }
+
+    public int Count => Pixels.Length;
+    public Color this[int index]
     {
-        public int Width { get; }
-        public int Height { get; }
-        public Color[] Pixels { get; }
+        get => Pixels[index];
+        set => Pixels[index] = value;
+    }
+    public IEnumerator<Color> GetEnumerator() => (IEnumerator<Color>)Pixels.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => Pixels.GetEnumerator();
 
-        public int Count => Pixels.Length;
-        public Color this[int index]
-        {
-            get => Pixels[index];
-            set => Pixels[index] = value;
-        }
-        public IEnumerator<Color> GetEnumerator() => (IEnumerator<Color>)Pixels.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => Pixels.GetEnumerator();
+    protected virtual int GetPixelOffset(int x, int y) => x + y * Width;
 
-        protected virtual int GetPixelOffset(int x, int y) => x + y * Width;
-
-        public Color this[Point point]
-        {
-            get => this[point.X, point.Y];
-            set => this[point.X, point.Y] = value;
-        }
-
-        public Color this[int x, int y]
-        {
-            get => Pixels[GetPixelOffset(x, y)];
-            set => Pixels[GetPixelOffset(x, y)] = value;
-        }
-
-        public ImageData(int width, int height)
-        {
-            Width = width;
-            Height = height;
-            Pixels = new Color[width * height];
-        }
-
-        public ImageData(int width, int height, Func<Point, Color> getPixel)
-            : this(width, height)
-        {
-            for (var x = 0; x < width; x++)
-                for (var y = 0; y < height; y++)
-                    this[x, y] = getPixel(new Point(x, y));
-        }
-
-        public ImageData(ImageData data) : this(data.Width, data.Height, pt => data[pt]) { }
-
-        public ImageData(SKImage img) : this(img.GetImageData()) { }
-
-        public ImageData(SKBitmap bmp) : this(bmp.GetImageData()) { }
+    public Color this[Point point]
+    {
+        get => this[point.X, point.Y];
+        set => this[point.X, point.Y] = value;
     }
 
+    public Color this[int x, int y]
+    {
+        get => Pixels[GetPixelOffset(x, y)];
+        set => Pixels[GetPixelOffset(x, y)] = value;
+    }
+
+    public ImageData(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Pixels = new Color[width * height];
+    }
+
+    public ImageData(int width, int height, Func<Point, Color> getPixel)
+        : this(width, height)
+    {
+        for (var x = 0; x < width; x++)
+            for (var y = 0; y < height; y++)
+                this[x, y] = getPixel(new Point(x, y));
+    }
+
+    public ImageData(ImageData data) : this(data.Width, data.Height, pt => data[pt]) { }
+
+    public ImageData(SKImage img) : this(img.GetImageData()) { }
+
+    public ImageData(SKBitmap bmp) : this(bmp.GetImageData()) { }
+
+    public static Endian SerializationEndian { get; set; } = Endian.Little;
+
+    public byte[] Serialize()
+    {
+        var buf = new List<byte>(8 + Count * 32);
+        buf.AddRange(Width.ToBytes(SerializationEndian));
+        buf.AddRange(Height.ToBytes(SerializationEndian));
+        foreach (var px in Pixels)
+        {
+            buf.Add(px.A);
+            buf.Add(px.R);
+            buf.Add(px.G);
+            buf.Add(px.B);
+        }
+        return buf.ToArray();
+    }
+
+    public static ImageData? Deserialize(byte[] bytes, int offset = 0)
+    {
+        if (bytes.Length - offset < 12)
+            return null;
+        var w = bytes.ToStruct<int>(offset, SerializationEndian);
+        offset += 4;
+        var h = bytes.ToStruct<int>(offset, SerializationEndian);
+        offset += 4;
+        if (w < 1 || h < 1)
+            return null;
+        var count = w * h;
+        if (bytes.Length - offset < count * 4)
+            return null;
+        var img = new ImageData(w, h);
+        for (var i = 0; i < count; i++)
+        {
+            img[i] = new Color(bytes[offset + 1], bytes[offset + 2], bytes[offset + 3], bytes[offset]);
+            offset += 4;
+        }
+        return img;
+    }
 }
