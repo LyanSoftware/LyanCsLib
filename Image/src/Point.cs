90a124060c0e9062f0c50654d862cb4b58ea9bd4
K <970227268@qq.com>
2026-06-25 19:36:49 +0800

    [Image]添加Size、Rect，完善Point

--- Image/src/Point.cs
+++ Image/src/Point.cs
@@ -4,10 +4,16 @@ using System.Text;
 
 namespace Lytec.Image;
 
-public readonly struct Point
+public readonly struct Point : IEquatable<Point>
 {
     public int X { get; }
     public int Y { get; }
 
     public Point(int x, int y) => (X, Y) = (x, y);
+
+    public override bool Equals(object? obj) => obj is Point point && Equals(point);
+    public bool Equals(Point other) => X == other.X && Y == other.Y;
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public static bool operator ==(Point left, Point right) => left.Equals(right);
+    public static bool operator !=(Point left, Point right) => !(left == right);
 }
