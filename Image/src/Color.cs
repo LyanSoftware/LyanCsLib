17976fa194f375bb5e35eb31c3980e9b945d15a3
K <970227268@qq.com>
2026-07-03 19:23:54 +0800

    [Image]完善Color相关代码

--- Image/src/Color.cs
+++ Image/src/Color.cs
@@ -17,6 +17,7 @@ public readonly partial struct Color : IEquatable<Color>
     public SKColor SKColor => new SKColor(R, G, B, A);
 
     public Color(byte r, byte g, byte b, byte a = 255) => (R, G, B, A) = (r, g, b, a);
+    public Color(int r, int g, int b, int a = 255) => (R, G, B, A) = ((byte)r, (byte)g, (byte)b, (byte)a);
     public Color(SKColor color) : this(color.Red, color.Green, color.Blue, color.Alpha) { }
     public Color(Color color) : this(color.R, color.G, color.B, color.A) { }
     public void Deconstruct(out byte r, out byte g, out byte b, out byte a) => (r, g, b, a) = (R, G, B, A);
--- Image/src/ColorJsonConverter.cs
+++ Image/src/ColorJsonConverter.cs
@@ -1,33 +1,63 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Text.RegularExpressions;
+using Lytec.Common;
 using Newtonsoft.Json;
 
 namespace Lytec.Image;
 
-public class ColorJsonConverter : JsonConverter<Color>
+public partial class ColorJsonConverter : JsonConverter<Color>
 {
     public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
     {
-        Color c(long v) => new Color((byte)(v >> 16), (byte)(v >> 8), (byte)v, (byte)(v >> 24));
-        switch (reader.Value)
+        static Color conv(int v, int len)
         {
-            case string str:
-                {
-                    if (str.StartsWith("#"))
-                        str = str[1..];
-                    if (uint.TryParse(str, out var v))
-                        return c(v);
-                    break;
-                }
-            case long v:
-                return c(v);
+            switch (len)
+            {
+                case 3: // RGB -> FFRRGGBB
+                    {
+                        var r = (v >> 8) & 0xF;
+                        var g = (v >> 4) & 0xF;
+                        var b = v & 0xF;
+                        return new Color((r << 4) | r, (g << 4) | g, (b << 4) | b, 0xFF);
+                    }
+                case 6: // RRGGBB -> FFRRGGBB
+                    return new Color((v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF);
+                case 4: // ARGB -> AARRGGBB
+                    {
+                        var a = (v >> 12) & 0xF;
+                        var r = (v >> 8) & 0xF;
+                        var g = (v >> 4) & 0xF;
+                        var b = v & 0xF;
+                        return new Color((r << 4) | r, (g << 4) | g, (b << 4) | b, (a << 4) | a);
+                    }
+                case 8: // AARRGGBB
+                    return new Color((v >> 16) & 0xFF, (v >> 8) & 0xFF, v & 0xFF, (v >> 24) & 0xFF);
+            }
+            throw new JsonSerializationException();
         }
-        return Color.Magenta;
+        if (reader.Value is string str)
+        {
+            if (str.Length > 0 && str[0] == '#')
+                str = str[1..];
+            switch (str.Length)
+            {
+                case 3:
+                case 4:
+                case 6:
+                case 8:
+                    if (uint.TryParse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var v))
+                        return conv((int)v, str.Length);
+                    break;
+            }
+        }
+        throw new JsonSerializationException($"Invalid color value '{reader.Value}'. Expected #RGB, #ARGB, #RRGGBB or #AARRGGBB.");
     }
 
     public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
     {
-        writer.WriteValue($"#{value:X8}");
+        writer.WriteValue($"#{value.A:X2}{value.R:X2}{value.G:X2}{value.B:X2}");
     }
 }
