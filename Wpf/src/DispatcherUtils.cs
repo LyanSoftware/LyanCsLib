4cb9cdda8849ceef3a9573bb5cbbe27ac92f21d8
K <970227268@qq.com>
2026-07-11 12:33:37 +0800

    [Wpf]调整DispatcherUtils

--- Wpf/src/DispatcherUtils.cs
+++ Wpf/src/DispatcherUtils.cs
@@ -9,58 +9,61 @@ using System.Windows.Threading;
 
 namespace Lytec.Wpf;
 
-public interface IUiObj { }
+public interface IUiObj
+{
+    Dispatcher Dispatcher { get; }
+}
 
 public static class DispatcherUtils
 {
-    public static Dispatcher Dispatcher => Application.Current.Dispatcher;
+    public static Dispatcher CurrentDispatcher => Application.Current.Dispatcher;
 
-    public static void Invoke(this IUiObj _, Action action)
+    public static void Invoke(this IUiObj obj, Action action)
     {
-        if (Dispatcher.CheckAccess())
+        if (obj.Dispatcher.CheckAccess())
             action();
-        else Dispatcher.Invoke(action);
+        else obj.Dispatcher.Invoke(action);
     }
     
-    public static void Invoke(this IUiObj _, Action action, DispatcherPriority priority)
-    => Dispatcher.Invoke(action, priority);
+    public static void Invoke(this IUiObj obj, Action action, DispatcherPriority priority)
+    => obj.Dispatcher.Invoke(action, priority);
 
-    public static T Invoke<T>(this IUiObj _, Func<T> action)
-    => Dispatcher.CheckAccess() ? action() : Dispatcher.Invoke(action);
+    public static T Invoke<T>(this IUiObj obj, Func<T> action)
+    => obj.Dispatcher.CheckAccess() ? action() : obj.Dispatcher.Invoke(action);
 
-    public static T Invoke<T>(this IUiObj _, Func<T> action, DispatcherPriority priority)
-    => Dispatcher.Invoke(action, priority);
+    public static T Invoke<T>(this IUiObj obj, Func<T> action, DispatcherPriority priority)
+    => obj.Dispatcher.Invoke(action, priority);
 
-    public static Task InvokeAsync(this IUiObj _, Action action)
+    public static Task InvokeAsync(this IUiObj obj, Action action)
     {
-        if (Dispatcher.CheckAccess())
+        if (obj.Dispatcher.CheckAccess())
         {
             action();
             return Task.CompletedTask;
         }
-        else return Dispatcher.InvokeAsync(action).Task;
+        else return obj.Dispatcher.InvokeAsync(action).Task;
     }
     
-    public static Task InvokeAsync(this IUiObj _, Action action, DispatcherPriority priority)
-    => Dispatcher.InvokeAsync(action, priority).Task;
+    public static Task InvokeAsync(this IUiObj obj, Action action, DispatcherPriority priority)
+    => obj.Dispatcher.InvokeAsync(action, priority).Task;
 
-    public static async Task<T> InvokeAsync<T>(this IUiObj _, Func<T> action)
-    => Dispatcher.CheckAccess() ? action() : await Dispatcher.InvokeAsync(action);
+    public static async Task<T> InvokeAsync<T>(this IUiObj obj, Func<T> action)
+    => obj.Dispatcher.CheckAccess() ? action() : await obj.Dispatcher.InvokeAsync(action);
     
-    public static Task<T> InvokeAsync<T>(this IUiObj _, Func<T> action, DispatcherPriority priority)
-    => Dispatcher.InvokeAsync(action, priority).Task;
+    public static Task<T> InvokeAsync<T>(this IUiObj obj, Func<T> action, DispatcherPriority priority)
+    => obj.Dispatcher.InvokeAsync(action, priority).Task;
 
-    public static async Task InvokeAsync(this IUiObj _, Func<Task> action)
-    => await (Dispatcher.CheckAccess() ? action() : await Dispatcher.InvokeAsync(action));
+    public static async Task InvokeAsync(this IUiObj obj, Func<Task> action)
+    => await (obj.Dispatcher.CheckAccess() ? action() : await obj.Dispatcher.InvokeAsync(action));
     
-    public static async Task InvokeAsync(this IUiObj _, Func<Task> action, DispatcherPriority priority)
-    => await await Dispatcher.InvokeAsync(action, priority);
+    public static async Task InvokeAsync(this IUiObj obj, Func<Task> action, DispatcherPriority priority)
+    => await await obj.Dispatcher.InvokeAsync(action, priority);
 
-    public static async Task<T> InvokeAsync<T>(this IUiObj _, Func<Task<T>> action)
-    => await (Dispatcher.CheckAccess() ? action() : await Dispatcher.InvokeAsync(action));
+    public static async Task<T> InvokeAsync<T>(this IUiObj obj, Func<Task<T>> action)
+    => await (obj.Dispatcher.CheckAccess() ? action() : await obj.Dispatcher.InvokeAsync(action));
     
-    public static async Task<T> InvokeAsync<T>(this IUiObj _, Func<Task<T>> action, DispatcherPriority priority)
-    => await await Dispatcher.InvokeAsync(action, priority);
+    public static async Task<T> InvokeAsync<T>(this IUiObj obj, Func<Task<T>> action, DispatcherPriority priority)
+    => await await obj.Dispatcher.InvokeAsync(action, priority);
     
     public static void Invoke(this DispatcherObject obj, Action action)
     {
