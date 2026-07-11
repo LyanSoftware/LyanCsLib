1d0c144df64c86e0d5aad6c4369613b71d5bcc35
K <970227268@qq.com>
2026-07-11 21:58:09 +0800

    [Wpf]微调逻辑

--- Wpf/src/FirstNonNullMultiConverter.cs
+++ Wpf/src/FirstNonNullMultiConverter.cs
@@ -11,14 +11,14 @@ namespace Lytec.Wpf;
 
 public class FirstNonNullMultiConverter : IMultiValueConverter
 {
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         foreach (var val in values)
         {
             if (val != null && val != DependencyProperty.UnsetValue)
                 return val;
         }
-        return Binding.DoNothing; // 或 null
+        return null; // 或 Binding.DoNothing
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
