83b95d520a2c00ed9fcfac09931685cb2008e52c
ly <lyansoftware@gmail.com>
2026-07-18 14:25:33 +0800

    [Common]GenerateHashAlgorithmExtensionsAttribute允许指定需生成的扩展函数名称

--- Common/src/Algorithm/GenerateHashAlgorithmExtensionsAttribute.cs
+++ Common/src/Algorithm/GenerateHashAlgorithmExtensionsAttribute.cs
@@ -1,10 +1,14 @@
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+[AttributeUsage(
+    AttributeTargets.Method | AttributeTargets.Property,
+    AllowMultiple = false,
+    Inherited = false)]
 public class GenerateHashAlgorithmExtensionsAttribute
     : Attribute
 {
     public delegate T ConvertHashResult<T>(byte[] bytes);
 
     public string? ConvertResultFuncName { get; set; }
+    public string? ExtensionMethodName { get; set; }
     public GenerateHashAlgorithmExtensionsAttribute() { }
     public GenerateHashAlgorithmExtensionsAttribute(string convertResultFuncName) => ConvertResultFuncName = convertResultFuncName;
 }
