d318e077a87b75704d6a3160e395b7f5ec7d22c9
ly <lyansoftware@gmail.com>
2026-09-02 18:14:08 +0800

    [AvaloniaUI]MenuItemExtensions添加Style<T>的链式调用扩展

--- AvaloniaUI/src/Mvu/MenuItemExtensions.cs
+++ AvaloniaUI/src/Mvu/MenuItemExtensions.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Declarative;
 using Avalonia.Styling;
 using Lytec.AvaloniaUI.Mvu.SimpleTheme;
 using System;
@@ -9,30 +10,46 @@ namespace Lytec.AvaloniaUI.Mvu;
 
 public static class MenuItemExtensions
 {
-    public static T ShowIconColumn<T>(
-        this T control,
-        bool value)
+    public static T ShowIconColumn<T>(this T control, bool value)
         where T : MenuItem
     {
         MenuItemLayout.SetShowIconColumn(control, value);
         return control;
     }
 
-    public static T EmptySubmenuArrowColumnWidth<T>(
-        this T control,
-        double value)
+    public static Style<T> ShowIconColumn<T>(this Style<T> style, bool value)
+        where T : MenuItem
+    {
+        style.Setters.Add(new Setter(MenuItemLayout.ShowIconColumnProperty, value));
+        return style;
+    }
+
+    public static T EmptySubmenuArrowColumnWidth<T>(this T control, double value)
         where T : MenuItem
     {
         MenuItemLayout.SetEmptySubmenuArrowColumnWidth(control, value);
         return control;
     }
 
-    public static T InputGestureTextTheme<T>(
-        this T control,
-        ControlTheme? value)
+    public static Style<T> EmptySubmenuArrowColumnWidth<T>(this Style<T> style, double value)
+        where T : MenuItem
+    {
+        style.Setters.Add(new Setter(MenuItemLayout.EmptySubmenuArrowColumnWidthProperty, value));
+        return style;
+    }
+
+    public static T InputGestureTextTheme<T>(this T control, ControlTheme? value)
         where T : MenuItem
     {
         MenuItemLayout.SetInputGestureTextTheme(control, value);
         return control;
     }
+
+    public static Style<T> InputGestureTextTheme<T>(this Style<T> style, ControlTheme? value)
+        where T : MenuItem
+    {
+        style.Setters.Add(new Setter(MenuItemLayout.InputGestureTextThemeProperty, value));
+        return style;
+    }
+
 }
