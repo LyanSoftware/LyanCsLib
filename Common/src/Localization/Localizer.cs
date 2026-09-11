e5a95cad70bb13864a87e8cbd21471875a56ca8d
ly <lyansoftware@gmail.com>
2026-09-11 13:54:49 +0800

    [Common]修正本地化输出

--- Common/src/Localization/Localizer.cs
+++ Common/src/Localization/Localizer.cs
@@ -44,7 +44,7 @@ public abstract class Localizer : ILocalizer
 
         try
         {
-            return (!hasTranslation || str.Arguments is null)
+            return str.Arguments is null
                 ? format
                 : Smart.Format(CurrentCulture, format, [str.Arguments]);
         }
